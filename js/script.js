f2b52e36b68eaee00acee0b5b26181aa7c6f8312
Ilya <fukodiaz@gmail.com>
2020-10-13 16:45:00 +0300

    Lesson 63: navigative dots for slider

--- js/script.js
+++ js/script.js
@@ -391,6 +391,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Slider
 	
 	const slides = document.querySelectorAll('.offer__slide'),
+			slider = document.querySelector('.offer__slider'),
 			prev = document.querySelector('.offer__slider-prev'),
 			next = document.querySelector('.offer__slider-next'),
 			total = document.querySelector('#total'),
@@ -419,6 +420,25 @@ window.addEventListener('DOMContentLoaded', () => {
 	slides.forEach(slide => {
 		slide.style.width = width;
 	});
+	// Slider(begining)
+
+	slider.style.position = 'relative';
+
+	const indicators = document.createElement('ol'),
+			dots = [];
+	indicators.classList.add('carousel-indicators');
+	slider.append(indicators);
+
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement('li');
+		dot.setAttribute('data-slide-to', i + 1);
+		dot.classList.add('dot');
+		if (i == 0) {
+			dot.style.opacity = 1;
+		}
+		indicators.append(dot);
+		dots.push(dot);
+	}
 
 	next.addEventListener('click', () => {
 		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
@@ -440,6 +460,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			current.textContent = slideIndex;
 		}
+
+		dots.forEach(dot => dot.style.opacity = '.5');
+		dots[slideIndex - 1].style.opacity = '1';
 	});
 
 	prev.addEventListener('click', () => {
@@ -462,6 +485,30 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			current.textContent = slideIndex;
 		}
+
+		dots.forEach(dot => dot.style.opacity = '.5');
+		dots[slideIndex - 1].style.opacity = '1';
+	});
+
+	//slider(end(dots))
+
+	dots.forEach(dot => {
+		dot.addEventListener('click', (e) => {
+			const slideTo = e.target.getAttribute('data-slide-to');
+			slideIndex = slideTo;
+			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			slidesField.style.transform = `translateX(-${offset}px)`;
+
+			dots.forEach(dot => dot.style.opacity = '.5');
+			dots[slideIndex - 1].style.opacity = '1';
+
+			if (slides.length < 10) {
+				current.textContent = `0${slideIndex}`;
+			} else {
+				current.textContent = slideIndex;
+			}
+
+		}); 
 	});
 
 
